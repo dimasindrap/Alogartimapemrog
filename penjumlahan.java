7c29a5cef222eb41d535102e936cfc918b8cdb1b
dimasindrap <93525439+dimasindrap@users.noreply.github.com>
2022-09-14 10:21:02 +0700

    Add files via upload

---
+++ penjumlahan.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class penjumlahan {
+    public static void main(String[] args) {
+        int x;
+        int y;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Masukkan Nilai x : ");
+        x = inp.nextInt();
+        System.out.print("Masukkan Nilai y : ");
+        y = inp.nextInt();
+
+        int hasil = x + y;
+
+        System.out.print("hasil = " + hasil);
+        
+
+    }
+
+}
